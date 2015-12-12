e5153b2ee842a3299a9c99e07fde5f660d75b173
Caitlin Stanton <caitlinstanton44@gmail.com>
2015-12-12 13:00:22 -0500

    Woooo

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, session, redirect,url_for, request
+
+import twitter, nyt
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/forum")
+def forum():
+    return render_template("forum.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/search",methods=["GET","POST"])
+def search():
+    if request.method=="POST":
+        twitter.search(request.form['searchterm'])
+        nyt.search(request.form['searchterm'])
+        f = open('tweets.csv','r')
+        tweets = f.read()
+        f.close()
+        tweets = tweets.decode('utf-8')
+        g = open('articles.csv','r')
+        articles = g.read()
+        g.close()
+        articles = articles.decode('utf-8')
+        return render_template("index.html",twitter=tweets,nyt=articles)
+
+    return render_template("search.html",twitter='No Search Has Been Done',nyt='No Search Has Been Done')                 
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.secret_key="Don't store this on github"
+    app.run()
