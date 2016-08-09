75aab19a0886526ebb3a3963e8dcdf573d05e5df
Caitlin Stanton <caitlinstanton44@gmail.com>
2016-08-09 01:47:10 -0400

    C

--- app.py
+++ app.py
@@ -6,29 +6,75 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
-def index():
+def homes():
     return render_template("index.html")
 
+@app.route("/thebasics")
+def thebasics():
+    return render_template("thebasics.html")
+
+@app.route("/politicalraces")
+def politicalraces():
+    return render_template("politicalraces.html")
+
 @app.route("/livechat")
-def forum():
+def livechat():
     return render_template("forum.html")
 
+@app.route("/search",methods=["GET","POST"])
+def search():
+    if request.method=="POST":
+        twitter.search(request.form['searchterm'])
+        nyt.search(request.form['searchterm'])
+        f = open('tweets.csv','r')
+        tweets = f.read()
+        f.close()
+        tweets = tweets.decode('utf-8')
+        g = open('articles.csv','r')
+        articles = g.read()
+        g.close()
+        articles = articles.decode('utf-8')
+        return render_template("search.html",twitter=tweets,nyt=articles)
+    return render_template("search.html",twitter='No Search Has Been Done',nyt='No Search Has Been Done')                 
+
 @app.route("/forum")
-def blog():
+def forum():
     blogs = db_methods.getPosts()
     if session.has_key("loggedIn") and session["loggedIn"]:
         if request.form.has_key("BlogID"):
-            return render_template("blog.html", loggedIn = True, username = session["username"], blogs = blogs, editing = request.form["BlogID"])
+            return render_template("forum.html", loggedIn = True, username = session["username"], blogs = blogs, editing = request.form["BlogID"])
         else:
             if request.form.has_key("edit"):
                 db_methods.editPost(request.form["edit"], request.form["editedID"])
                 blogs = db_methods.getPosts()
-            return render_template("blog.html", loggedIn = True, username = session["username"], blogs = blogs, editing = "-1")
+            return render_template("forum.html", loggedIn = True, username = session["username"], blogs = blogs, editing = "-1")
     else:
         if request.form.has_key("BlogID"):
             return redirect(url_for("login"))
         else:
-            return render_template("blog.html", loggedIn = False, blogs = blogs)
+            return render_template("forum.html", loggedIn = False, blogs = blogs)
+
+@app.route("/createpost")
+def createpost():
+    if session.has_key("loggedIn") and session["loggedIn"]:
+        return render_template("createpost.html", username = session["username"])
+    else:
+        return redirect(url_for("login"))           
+        
+@app.route("/myposts", methods = ["GET", "POST"])
+def myposts():
+    if session.has_key("loggedIn") and session["loggedIn"]:
+        userPosts = db_methods.getUserPosts(session["username"])
+        if request.form.has_key("post") and request.form["post"] != "":
+            db_methods.addPost(request.form["title"], request.form["post"], session["username"])
+        elif request.form.has_key("BlogID"):
+            return render_template("myposts.html", username = session["username"], userPosts = userPosts, editing = request.form["BlogID"])
+        elif request.form.has_key("edit"):
+            db_methods.editUserPost(request.form["edit"], request.form["editedID"], session["username"])
+        userPosts = db_methods.getUserPosts(session["username"])
+        return render_template("myposts.html", username = session["username"], userPosts = userPosts)
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
@@ -69,46 +115,6 @@ def logout():
     if session.has_key("loggedIn") and session["loggedIn"]:
         session["loggedIn"] = False
     return redirect(url_for("blog"))
-
-@app.route("/createpost")
-def createpost():
-    if session.has_key("loggedIn") and session["loggedIn"]:
-        return render_template("createpost.html", username = session["username"])
-    else:
-        return redirect(url_for("login"))			
-		
-@app.route("/myposts", methods = ["GET", "POST"])
-def myposts():
-    if session.has_key("loggedIn") and session["loggedIn"]:
-        userPosts = db_methods.getUserPosts(session["username"])
-        if request.form.has_key("post") and request.form["post"] != "":
-            db_methods.addPost(request.form["title"], request.form["post"], session["username"])
-        elif request.form.has_key("BlogID"):
-            return render_template("myposts.html", username = session["username"], userPosts = userPosts, editing = request.form["BlogID"])
-        elif request.form.has_key("edit"):
-            db_methods.editUserPost(request.form["edit"], request.form["editedID"], session["username"])
-        userPosts = db_methods.getUserPosts(session["username"])
-        return render_template("myposts.html", username = session["username"], userPosts = userPosts)
-    else:
-        return redirect(url_for("login"))
-		
-@app.route("/search",methods=["GET","POST"])
-def search():
-    if request.method=="POST":
-        twitter.search(request.form['searchterm'])
-        nyt.search(request.form['searchterm'])
-        f = open('tweets.csv','r')
-        tweets = f.read()
-        f.close()
-        tweets = tweets.decode('utf-8')
-        g = open('articles.csv','r')
-        articles = g.read()
-        g.close()
-        articles = articles.decode('utf-8')
-        return render_template("search.html",twitter=tweets,nyt=articles)
-
-    return render_template("search.html",twitter='No Search Has Been Done',nyt='No Search Has Been Done')                 
-
 
 if __name__ == "__main__":
     app.debug = True
